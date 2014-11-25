f0f6211c04e6217547bb9a54eb4e352d92aa4acb
Terence Liew <monkeyintomischief@hotmail.com>
2014-11-26 06:01:41 +1100

    Redirecting functionality
    Early version of redirecting from creation to index page, added flash
    messages.
    
    In the next version appropriate fields will be implemented for the form
    input.

--- app/controllers/basketball_controller.rb
+++ app/controllers/basketball_controller.rb
@@ -2,25 +2,48 @@ class BasketballController < ApplicationController
 	# index, show, new and edit have views
 	# create, update and destroy only perform processing
 	
+	# Trigger only in create and update methods?
+	before_filter :check_for_cancel, :only => [:create, :update]
+	def check_for_cancel
+		# if value == Cancel, redirect to index
+		if params[:commit] == "Cancel"
+			redirect_to basketball_index_path
+		end
+	end
+	
+	def basketball_params
+		params.require(:basketball).permit(:time, :teams, :location, :status, :winner, :score)
+	end
+	
 	def index
 		# create an instance variable
 		# Store all of the basketball results inside the variable
 		@basketballs = Basketball.all
 	end
 	
-	# To show an individual basketball
-	def show
-	
-	end
-	
 	# Display a form to create a new basketball
 	def new
-	
+		# empty basketball object
+		@basketball = Basketball.new
 	end
 	
 	# Form from the new action will submit and save to DB
 	def create
-	
+		# declare new variable basketball obj to store result
+		# Grab form information through params :basketball
+		#@basketball = Basketball.new(params[:basketball])
+		@basketball = Basketball.new(basketball_params)
+		
+		# Save the basketball obj to DB
+		# @basketball.save will return true or false, so we evaluate it
+		if @basketball.save
+			# Redirect to index, create a flash message to display in index
+			redirect_to basketball_index_path, :notice => "Success! Event was saved"
+		else
+			# if not able to save Basketball
+			# Render the new page
+			render "new"
+		end
 	end
 	
 	# Display a form for editing a basketball
--- app/models/basketball.rb
+++ app/models/basketball.rb
@@ -1,3 +1,4 @@
 class Basketball < ActiveRecord::Base
+	# Rails 4 uses strong parameters now
 	#attr_accessible :time, :teams, :location, :status, :winner, :score
 end
