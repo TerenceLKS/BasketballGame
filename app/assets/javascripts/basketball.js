d1422c8d5f9d8ac63ec9c6fa8eef3e05df61999d
Terence Liew <monkeyintomischief@hotmail.com>
2014-11-26 23:28:29 +1100

    [Update, v1.2] Create new Basketball event
    Extracted comments to list changes from the JavaScript file
    (basketball.js)
    
    Changes:
    - Added validation check for input only for score in the basketball_new
    page
    
    Assumptions:
      - Date, Time, Status, Winner does not have to be set
    immediately. Can be edited later by user
      - Date and Time is provided
    in a controlled input, hacking not considered
      - Only score is
    validated

--- app/assets/javascripts/basketball.js
+++ app/assets/javascripts/basketball.js
@@ -3,6 +3,37 @@
 
 function validateform()
 {
-	alert("test");
-	return true;
+	/*
+	Assumptions:
+		- Date, Time, Status, Winner does not have to be set immediately. Can be edited later by user
+		- Date and Time is provided in a controlled input, hacking not considered
+		- Only score is validated
+	*/
+	var isValid = true;
+	var message = "";
+	var score;
+	score = document.getElementById("basketball_score").value;
+	
+	// Score validation
+	if ( isNaN(score) ) // Check if not a valid number
+	{
+		message += "Score is not a valid number";
+		isValid = false;
+	}
+	else if ( !(score % 1 === 0) ) // Else if it is, is it a whole num?
+	{
+		message += "Score must be a whole number";
+		isValid = false;
+	}
+	
+	// If isValid is still true, accept inputs
+	if ( isValid ) 
+	{
+		return true;
+	}
+	else // Else fail, alert problem and return false
+	{
+		alert(message);
+		return false;
+	}
 }
